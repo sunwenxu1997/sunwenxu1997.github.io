79a21060f749ef6d83c2ce57ba5d75c43b284548
孙文旭 <wenxu.sun@opple.com>
2023-08-30 17:30:12 +0800

    update

--- src/views/cssHtml/css-碰撞检测.vue
+++ src/views/cssHtml/css-碰撞检测.vue
@@ -11,64 +11,90 @@ export default {
     animation-composition: add;              // 表示动画值追加
     animation-composition: accumulate; // 表示动画值累加
  */
+const num = 10
 </script>
 <template>
-  <div class="dot one"></div>
-  <div class="dot two"></div>
-  <svg width="0" height="0" viewBox="0 0 16 16">
-    <clipPath id="clipPath">
-      <path
-        transform="scale(4)"
-        d="M8 0c-.787 0-1.292.592-1.572 1.151A4.347 4.347 0 0 0 6 3v3.691l-2 1V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.191l-1.17.585A1.5 1.5 0 0 0 0 10.618V12a.5.5 0 0 0 .582.493l1.631-.272.313.937a.5.5 0 0 0 .948 0l.405-1.214 2.21-.369.375 2.253-1.318 1.318A.5.5 0 0 0 5.5 16h5a.5.5 0 0 0 .354-.854l-1.318-1.318.375-2.253 2.21.369.405 1.214a.5.5 0 0 0 .948 0l.313-.937 1.63.272A.5.5 0 0 0 16 12v-1.382a1.5 1.5 0 0 0-.83-1.342L14 8.691V7.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v.191l-2-1V3c0-.568-.14-1.271-.428-1.849C9.292.591 8.787 0 8 0Z"
-      />
-    </clipPath>
-  </svg>
+  <!-- 通过控制动画延迟时间和透明度，实现运动模糊轨迹效果 -->
+  <div
+    class="dot"
+    v-for="item in num"
+    :key="item"
+    :style="`animation-delay: ${0.02 * item}s; opacity: ${(num - item) * 0.05};`"
+  ></div>
+  <div
+    class="dot two"
+    v-for="item in num"
+    :key="item"
+    :style="`animation-delay: ${0.02 * item}s; opacity: ${(num - item) * 0.05};`"
+  ></div>
+  <div
+    class="dot three"
+    v-for="item in num"
+    :key="item"
+    :style="`animation-delay: ${0.02 * item}s; opacity: ${(num - item) * 0.05};`"
+  ></div>
+  <div
+    class="dot four"
+    v-for="item in num"
+    :key="item"
+    :style="`animation-delay: ${0.02 * item}s; opacity: ${(num - item) * 0.05};`"
+  ></div>
 </template>
 <style scoped lang="scss">
-$dotSize: 64px;
+$dotSize: 80px;
 .dot {
-  clip-path: url('#clipPath');
-  width: $dotSize;
-  height: $dotSize;
   position: absolute;
   top: 0;
   left: 0;
+  width: $dotSize;
+  height: $dotSize;
+  border-radius: 50%;
   animation-composition: accumulate;
   background-color: #409eff;
-}
-.dot.one {
   animation:
-    horizontal 3s linear infinite -1s alternate,
-    vertical 5s linear infinite -2s alternate,
-    colorX 24s infinite -1s steps(8),
-    // 24 = 8 * 3
-    colorY 35s infinite -2s steps(7); // 35 = 7 * 5
-}
-.dot.two {
-  background-color: #b940ff;
-  animation-delay: 3s;
-  animation:
-    horizontal 3s linear infinite alternate,
+    horizontal 2s linear infinite alternate,
     vertical 5s linear infinite alternate,
-    colorX 24s infinite steps(8),
+    // 24 = 8 * 2
+    colorX 16s infinite steps(8),
+    // 35 = 7 * 5
     colorY 35s infinite steps(7);
 }
-
+.dot.two {
+  animation:
+    horizontal 2s linear infinite alternate-reverse,
+    vertical 5s linear infinite alternate-reverse,
+    // 24 = 8 * 2
+    colorX 16s infinite steps(8) alternate-reverse,
+    // 35 = 7 * 5
+    colorY 35s infinite steps(7) alternate-reverse;
+}
+.dot.three {
+  animation:
+    horizontal 2s linear infinite alternate-reverse,
+    vertical 5s linear infinite alternate,
+    // 24 = 8 * 2
+    colorX 16s infinite steps(8) alternate-reverse,
+    // 35 = 7 * 5
+    colorY 35s infinite steps(7) alternate;
+}
+.dot.four {
+  animation:
+    horizontal 2s linear infinite alternate,
+    vertical 5s linear infinite alternate-reverse,
+    // 24 = 8 * 2
+    colorX 16s infinite steps(8) alternate,
+    // 35 = 7 * 5
+    colorY 35s infinite steps(7) alternate-reverse;
+}
 /* 水平移动 */
 @keyframes horizontal {
-  0% {
-    transform: translateX(0);
-  }
-  100% {
+  to {
     transform: translateX(calc(100vw - #{$dotSize}));
   }
 }
 // 纵向移动
 @keyframes vertical {
-  0% {
-    transform: translateY(0);
-  }
-  100% {
+  to {
     transform: translateY(calc(100vh - #{$dotSize}));
   }
 }
