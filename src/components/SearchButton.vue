cbc536919f855252fb1ff19fdacf6b10493fbefc
15518270529 <1291397693@qq.com>
2023-11-01 22:45:25 +0800

    悬浮搜索优化

--- src/components/SearchButton.vue
+++ src/components/SearchButton.vue
@@ -4,83 +4,75 @@ import { ref, reactive, onMounted, onUnmounted } from 'vue'
 import { gsap } from 'gsap'
 const $emit = defineEmits(['input', 'search'])
 const searchBtnRef = ref(null),
-  searchInputShow = ref(false),
-  searchInputRef = ref(null),
-  searchInputValue = ref('')
+    searchInputShow = ref(false),
+    searchInputRef = ref(null),
+    searchInputValue = ref('')
 let mouseDownTimeout = null
 onMounted(() => {
-  document.body.style.cursor = 'none'
-  window.addEventListener('mousemove', mouseSearchBtnListener)
-  window.addEventListener('mousedown', mousedownSearchBtnListener)
-  window.addEventListener('mouseup', function () {
-    clearTimeout(mouseDownTimeout)
-  })
-  window.addEventListener('keydown', keydownSearchBtnListener)
+    document.body.style.cursor = 'none'
+    window.addEventListener('mousemove', mousemoveSearchBtnListener)
+    window.addEventListener('mousedown', mousedownSearchBtnListener)
+    window.addEventListener('mouseup', function () {
+        clearTimeout(mouseDownTimeout)
+    })
+    window.addEventListener('keydown', keydownSearchBtnListener)
 })
 onUnmounted(() => {
-  document.body.style.cursor = 'auto'
-  window.removeEventListener('mousemove', mouseSearchBtnListener)
-  window.removeEventListener('mousedown', mousedownSearchBtnListener)
-  window.removeEventListener('keydown', keydownSearchBtnListener)
+    document.body.style.cursor = 'auto'
+    window.removeEventListener('mousemove', mousemoveSearchBtnListener)
+    window.removeEventListener('mousedown', mousedownSearchBtnListener)
+    window.removeEventListener('keydown', keydownSearchBtnListener)
 })
 const keydownSearchBtnListener = (e) => {
-  if (e.key === 'Enter') {
-    // 在这里执行回车键按下后的操作
-    console.log('回车键被按下')
-    $emit('search', searchInputValue.value)
-    searchInputShow.value = false
-    searchInputValue.value = ''
-    searchInputRef.value.blur()
-  }
-  if (e.key === 'Escape') {
-    // 在这里执行 Esc 键按下后的操作
-    console.log('Esc 键被按下')
-    searchInputShow.value = false
-    searchInputValue.value = ''
-    searchInputRef.value.blur()
-  }
+    if (e.key === 'Enter') {
+        // 在这里执行回车键按下后的操作
+        console.log('回车键被按下')
+        if (!searchInputValue.value) searchInputShow.value = false
+        $emit('input', searchInputValue.value)
+        searchInputRef.value.blur()
+    }
+    if (e.key === 'Escape') {
+        // 在这里执行 Esc 键按下后的操作
+        console.log('Esc 键被按下')
+        searchInputShow.value = false
+        $emit('input', searchInputValue.value)
+        searchInputRef.value.blur()
+    }
 }
 const mousedownSearchBtnListener = () => {
-  mouseDownTimeout = setTimeout(function () {
-    // 在这里执行按下 500ms 秒钟后的操作
-    console.log('鼠标按下 500ms 秒钟')
-    searchInputShow.value = true
-    searchInputRef.value.focus()
-  }, 500) 
+    mouseDownTimeout = setTimeout(function () {
+        // 在这里执行按下 500ms 秒钟后的操作
+        console.log('鼠标按下 500ms 秒钟')
+        searchInputShow.value = true
+        searchInputRef.value.focus()
+    }, 500)
 }
-const mouseSearchBtnListener = (e) => {
-  let el = searchBtnRef.value
-  //圆灯相对于 容器的坐标位置
-  let x = e.clientX - el.getBoundingClientRect().width / 2
-  let y = e.clientY - el.getBoundingClientRect().height / 2
-  gsap.to(el, {
-    left: x,
-    top: y,
-    opacity: 0.8,
-    ease: 'back(0.1)',
-    duration: 0.5
-  })
+const mousemoveSearchBtnListener = (e) => {
+    if (!searchInputValue.value) searchInputShow.value = false
+    let el = searchBtnRef.value
+    //圆灯相对于 容器的坐标位置
+    let x = e.clientX - el.getBoundingClientRect().width / 2
+    let y = e.clientY - el.getBoundingClientRect().height / 2
+    gsap.to(el, {
+        left: x,
+        top: y,
+        opacity: 0.8,
+        ease: 'back(0.1)',
+        duration: 0.5
+    })
 }
 </script>
 <template>
-  <div
-    ref="searchBtnRef"
-    class="fixed opacity-0 transition top-0 left-0 z-50 pointer-events-none min-w-[2.5rem] min-h-[2.5rem] rounded-full bg-white flex justify-center items-center"
-    :class="searchInputShow ? 'px-2' : 'px-0'"
-  >
-    <svg fill="#000000" class="icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M16.9994165,16.2923098 L20.8535534,20.1464466 C21.0488155,20.3417088 21.0488155,20.6582912 20.8535534,20.8535534 C20.6582912,21.0488155 20.3417088,21.0488155 20.1464466,20.8535534 L16.2923098,16.9994165 C14.8819612,18.2444908 13.0292099,19 11,19 C6.581722,19 3,15.418278 3,11 C3,6.581722 6.581722,3 11,3 C15.418278,3 19,6.581722 19,11 C19,13.0292099 18.2444908,14.8819612 16.9994165,16.2923098 Z M11,18 C14.8659932,18 18,14.8659932 18,11 C18,7.13400675 14.8659932,4 11,4 C7.13400675,4 4,7.13400675 4,11 C4,14.8659932 7.13400675,18 11,18 Z"
-      />
-    </svg>
-    <input
-      ref="searchInputRef"
-      class="select-none outline-none border-0 transition-all"
-      :class="searchInputShow ? 'w-20 px-2' : 'w-0 p-0'"
-      type="text"
-      v-model="searchInputValue"
-      @input="$emit('input', searchInputValue)"
-    />
-  </div>
+    <div ref="searchBtnRef"
+        class="fixed opacity-0 transition top-0 left-0 z-50 pointer-events-none min-w-[2.5rem] min-h-[2.5rem] rounded-full bg-white flex justify-center items-center"
+        :class="searchInputShow ? 'px-2' : 'px-0'">
+        <svg fill="#000000" class="icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M16.9994165,16.2923098 L20.8535534,20.1464466 C21.0488155,20.3417088 21.0488155,20.6582912 20.8535534,20.8535534 C20.6582912,21.0488155 20.3417088,21.0488155 20.1464466,20.8535534 L16.2923098,16.9994165 C14.8819612,18.2444908 13.0292099,19 11,19 C6.581722,19 3,15.418278 3,11 C3,6.581722 6.581722,3 11,3 C15.418278,3 19,6.581722 19,11 C19,13.0292099 18.2444908,14.8819612 16.9994165,16.2923098 Z M11,18 C14.8659932,18 18,14.8659932 18,11 C18,7.13400675 14.8659932,4 11,4 C7.13400675,4 4,7.13400675 4,11 C4,14.8659932 7.13400675,18 11,18 Z" />
+        </svg>
+        <input ref="searchInputRef" class="select-none outline-none border-0 transition-all"
+            :class="searchInputShow ? 'w-20 px-2' : 'w-0 p-0'" type="text" v-model="searchInputValue"
+            @input="$emit('input', searchInputValue)" />
+    </div>
 </template>
 <style lang="scss" scoped></style>
