6a25d3b6ee6721ce79f5a19cb236ca3b3fd5b385
孙文旭 <wenxu.sun@opple.com>
2023-12-29 11:48:23 +0800

    update

--- src/views/works/other/鼠标吸附按钮效果.vue
+++ src/views/works/other/鼠标吸附按钮效果.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="overall">
     <div class="container">
-      <div class="button one" cursor="link">我很粘人</div>
-      <!-- <div class="button two" cursor="link">button2</div>
-      <div class="button three" cursor="link">button3</div> -->
+      <div class="button one" cursor="link">button1</div>
+      <div class="button two" cursor="link">button2</div>
+      <div class="button three" cursor="link">button3</div>
       <div class="cursor"></div>
     </div>
   </div>
@@ -11,56 +11,71 @@
 
 <script>
 export default {
+  cover: 'https://iili.io/JRSzVKg.gif',
+  info: ` 
+    <p>多用于导航栏文字效果，让人感觉有种吸附的效果，主要用的就是监听鼠标得移入移出。</p>
+    <p>知识点：</p>
+    <p>1. <code>addEventListener</code>监听进入按钮时得 mousemove mouseleave</p>
+    <p>2. <code>rotate3d(x, y, z, angle)</code>
+       x：表示绕着X轴旋转的角度。
+       y：表示绕着Y轴旋转的角度。
+       z：表示绕着Z轴旋转的角度。
+       angle：表示旋转的角度，单位为度（deg）</p>
+  
+    `,
   mounted() {
-    this.init();
+    this.init()
   },
   methods: {
     init() {
-      this.cursor();
-      this.mousemove();
-      this.mouseleave();
+      this.cursor()
+      this.buttonMouseListener()
     },
-    //鼠标执行方法
-    cursor() {
-      let cursor = document.querySelector(".cursor");
-      document.addEventListener("mousemove", function (e) {
-        cursor.style.left = e.clientX - 25 + "px";
-        cursor.style.top = e.clientY - 25 + "px";
-      });
-    },
-    mousemove() {
-      let buttons = document.querySelectorAll(".button");
+    // 按钮粘性效果实现
+    buttonMouseListener() {
+      let buttons = document.querySelectorAll('.button')
       buttons.forEach((el) => {
-        el.addEventListener("mousemove", function (e) {
+        // 移入到按钮时
+        el.addEventListener('mousemove', function (e) {
           /* getBoundingClientRect用于获取某个元素相对于视窗的位置集合。集合中有top, right, bottom, left等属性 */
           //   https://www.jianshu.com/p/824eb6f9dda4
-          const pos = el.getBoundingClientRect();
-          const mx = e.clientX - pos.left - pos.width / 2;
-          const my = e.clientY - pos.top - pos.height / 2;
+          const pos = el.getBoundingClientRect()
+          const mx = e.clientX - pos.left - pos.width / 2
+          const my = e.clientY - pos.top - pos.height / 2
 
-          el.style.transform =
-            "translate(" + mx * 0.15 + "px, " + my * 0.3 + "px)";
-          el.style.transform +=
-            "rotate3d(" + mx * -0.1 + ", " + my * -0.3 + ", 0, 12deg)";
-        });
-      });
+          // 水平垂直偏移
+          el.style.transform = `translate(${mx * 0.3}px,${my * 0.3}px)`
+          // 3d旋转偏移
+          // rotate3d(x, y, z, angle)
+          /**
+           * x：表示绕着X轴旋转的角度。取值范围为[-1, 1]，其中-1表示绕着X轴旋转180度，1表示绕着X轴旋转180度。
+           * y：表示绕着Y轴旋转的角度。取值范围为[-1, 1]，其中-1表示绕着Y轴旋转180度，1表示绕着Y轴旋转180度。
+           * z：表示绕着Z轴旋转的角度。取值范围为[-1, 1]，其中-1表示绕着Z轴旋转180度，1表示绕着Z轴旋转180度。
+           * angle：表示旋转的角度，单位为度（deg）
+           */
+          el.style.transform += `rotate3d(${mx * -0.1},${my * -0.3},0,20deg)`
+        })
+        // 移出按钮时
+        el.addEventListener('mouseleave', function () {
+          el.style.transform = 'translate3d(0px, 0px, 0px)'
+          el.style.transform = 'rotate3d(0, 0, 0, 0deg)'
+        })
+      })
     },
-    mouseleave() {
-      let buttons = document.querySelectorAll(".button");
-      buttons.forEach((el) => {
-        el.addEventListener("mouseleave", function (e) {
-          el.style.transform = "translate3d(0px, 0px, 0px)";
-          el.style.transform = "rotate3d(0, 0, 0, 0deg)";
-        });
-      });
-    },
-  },
-};
+    //鼠标样式背景跟随
+    cursor() {
+      let cursor = document.querySelector('.cursor')
+      document.addEventListener('mousemove', function (e) {
+        cursor.style.left = e.clientX - 10 + 'px'
+        cursor.style.top = e.clientY - 10 + 'px'
+      })
+    }
+  }
+}
 </script>
-<style >
-</style>
+<style></style>
 <style lang="scss" scoped>
-$cursorSize: 50px;
+$cursorSize: 20px;
 .overall {
   background: #dbfbff;
 }
@@ -77,17 +92,20 @@ $cursorSize: 50px;
   height: $cursorSize;
   border-radius: 50%;
   border: 2px solid #ccc;
-  transition: transform 0.3s ease, background 0.3s ease, border-color 0.3s ease;
+  transition:
+    transform 0.3s ease,
+    background 0.3s ease,
+    border-color 0.3s ease;
 }
 /* 鼠标放在按钮上 仿鼠标标签的样式变化 */
-[cursor="link"]:hover ~ .cursor {
+[cursor='link']:hover ~ .cursor {
   mix-blend-mode: difference;
   transform: scale(1.5);
   background: #fff;
   border-color: #fff;
 }
 /* 鼠标点击按钮  仿鼠标标签的样式变化 */
-[cursor="link"]:active ~ .cursor {
+[cursor='link']:active ~ .cursor {
   transform: scale(0.8);
 }
 .container {
@@ -99,6 +117,8 @@ $cursorSize: 50px;
   height: 100%;
   align-items: center;
   justify-content: center;
+  transform-style: preserve-3d;
+  perspective: 500px;
 }
 .button {
   border-radius: 2px;
@@ -107,12 +127,15 @@ $cursorSize: 50px;
   display: inline-block;
   padding: 10px 20px;
   color: white;
-  font-size: 14px;
+  font-size: 20px;
   font-weight: 200;
   margin: 0 20px;
   outline: none;
   user-select: none;
-  transition: transform 0.1s linear, color 0.1s linear, background 0.15s linear;
+  transition:
+    transform 0.1s linear,
+    color 0.1s linear,
+    background 0.15s linear;
 }
 .button.one {
   background: #4f29f0;
