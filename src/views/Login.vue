3f01b86d16e039f31a6962c29cf1be9edcfe9b8b
孙文旭 <wenxu.sun@opple.com>
2025-07-01 17:26:38 +0800

    ✨登录页

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center">
-    <div class="w-full max-w-sm">
+  <!-- 背景有who字样充满整个屏幕 -->
+  <div
+    style="-webkit-text-stroke: 0.02rem #c3c3c3"
+    class="absolute top-0 left-0 z-0 overflow-hidden w-full h-full flex items-center justify-center"
+  >
+    <span class="text-[45vmax] text-transparent bg-clip-text font-bold rotate-90 sm:rotate-12 relative left-6 sm:left-0"
+      >WHO</span
+    >
+  </div>
+  <div class="h-dvh flex items-center justify-center relative z-10 mix-blend-overlay">
+    <div class="w-3/4 sm:w-full max-w-sm">
       <!-- WHO Logo -->
       <div class="text-center mb-8">
-        <h1 class="text-2 font-bold text-black tracking-wider font-[Rajdhani]">WHO.</h1>
+        <h1 class="text-lg font-bold text-black tracking-wider font-[Rajdhani] mt-0">WHO.</h1>
       </div>
 
       <!-- Login Form -->
@@ -15,7 +24,7 @@
             v-model="accountValue"
             type="text"
             placeholder="账号"
-            class="w-full h-12 px-6 box-border border-4 border-black text-black rounded-none focus:outline-none transition-all duration-300 ease-in-out"
+            class="w-full h-12 px-6 bg-transparent box-border border-2 border-black text-black rounded-none focus:outline-none transition-all duration-300 ease-in-out"
             required
             @keydown.prevent.enter="toLogin"
           />
@@ -28,7 +37,7 @@
             v-model="passwordValue"
             type="password"
             placeholder="密码"
-            class="w-full h-12 px-6 box-border border-4 border-black text-black rounded-none focus:outline-none transition-all duration-300 ease-in-out"
+            class="w-full h-12 px-6 bg-transparent box-border border-2 border-black text-black rounded-none focus:outline-none transition-all duration-300 ease-in-out"
             required
             @keydown.prevent.enter="toLogin"
           />
@@ -38,7 +47,7 @@
         <button
           @click="toLogin"
           type="submit"
-          class="w-full h-12 bg-black text-white hover:bg-gray-800 transition-all duration-300 ease-in-out hover:shadow-md hover:shadow-black/20 active:scale-[0.99] font-bold cursor-pointer rounded-none mt-6"
+          class="w-full h-12 border-none bg-black text-white hover:bg-gray-800 transition-all duration-300 ease-in-out hover:shadow-md hover:shadow-black/20 active:scale-[0.99] font-bold cursor-pointer rounded-none mt-6"
         >
           登 录
         </button>
