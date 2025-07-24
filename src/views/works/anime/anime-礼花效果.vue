8e855305de308fa7571fe9c6d82de643a79426d8
孙文旭 <wenxu.sun@opple.com>
2025-07-24 16:12:13 +0800

    ⚡

--- src/views/works/anime/anime-礼花效果.vue
+++ src/views/works/anime/anime-礼花效果.vue
@@ -4,39 +4,26 @@ import { animate, createAnimatable, utils, createTimer } from 'animejs'
 onMounted(() => {
   const [$body] = utils.$('.body')
   let bgFlowerArr = []
-  // 速度
-  const bgFlowerSpeed = 0.3
-  const bgFlowerNum = 200
-  const bgFlowerSize = 10
+
+  const bgFlowerSpeed = 0.3 // 背景礼花飘落速度
+  const bgFlowerNum = 200 // 背景礼花数量
+  const bgFlowerSize = 10 // 背景礼花大小
+
+  const [$button] = utils.$('.button')
+  const [$animeBox] = utils.$('.anime-box')
   // 创建背景礼花下落动画
   const createBgFlower = () => {
     const bounds = $body.getBoundingClientRect()
     const { width, height, left, top } = bounds
     // console.log(width)
     for (let i = 0; i < bgFlowerNum; i++) {
-      const $el = document.createElement('div')
       const scale = utils.random(0.2, 1.2, 1)
-      utils.set($el, {
-        width: `${bgFlowerSize * scale}px`,
-        height: `${bgFlowerSize * scale}px`,
-        position: 'absolute',
-        top: `${utils.random(-50, -20)}px`,
+      const $el = createFireworksDot({
         left: `${utils.random(0, width)}px`,
-        transformStyle: 'preserve-3d',
-        background: utils.randomPick([
-          '#dce90d',
-          '#0de9be',
-          '#0dbce9',
-          '#e9530d',
-          '#e90d59',
-          '#ffffff'
-        ]),
-        borderRadius: utils.randomPick(['50%', '0%']),
-        userSelect: 'none'
+        top: `${utils.random(-50, -20)}px`,
+        scale
       })
-      $el.classList.add('bg-flower')
       $body.appendChild($el)
-
       const animation = animate($el, {
         translateX: [
           {
@@ -59,7 +46,75 @@ onMounted(() => {
       bgFlowerArr.push({ $el, animation })
     }
   }
+
+  // 创建公用小花瓣
+  const createFireworksDot = ({ top, left, scale }) => {
+    const $el = document.createElement('div')
+    scale = scale || utils.random(0.2, 1.2, 1)
+    utils.set($el, {
+      width: `${bgFlowerSize * scale}px`,
+      height: `${bgFlowerSize * scale}px`,
+      position: 'absolute',
+      top,
+      left,
+      transformStyle: 'preserve-3d',
+      background: utils.randomPick([
+        '#dce90d',
+        '#0de9be',
+        '#0dbce9',
+        '#e9530d',
+        '#e90d59',
+        '#ffffff'
+      ]),
+      borderRadius: utils.randomPick(['50%', '0%']),
+      userSelect: 'none'
+    })
+    $el.classList.add('bg-flower')
+    return $el
+  }
+
+  // 点击按钮时执行礼花爆开动画
+  const createButtonAnimation = () => {
+    const bounds = $animeBox.getBoundingClientRect()
+    const { width, height, left, top } = bounds
+    const hw = width / 2
+    const hh = height / 2
+    for (let i = 0; i < 20; i++) {
+      const $el = createFireworksDot({
+        left: `${hw}px`,
+        top: `${hh}px`
+      })
+      $animeBox.appendChild($el)
+      const scale = utils.random(0.2, 1.2, 1)
+      animate($el, {
+        translateX: [
+          {
+            to: utils.random(-50, 50),
+            ease: 'out'
+          },
+          { to: utils.random(-100, 100), ease: 'inOut(2)' }
+        ],
+        translateY: [
+          {
+            to: utils.random(-200, 0),
+            ease: 'out'
+          },
+          { to: utils.random(height, height + 50), ease: 'inOut(2)' }
+        ],
+        duration: 1000 * (1 - scale) + 1000,
+        rotate: `${utils.random(1, 3)}turn`,
+        rotateX: `${utils.random(1, 3)}turn`,
+        rotateY: `1turn`,
+        scale: [1, 1.2, 1, 0.3],
+        opacity: [1, 1, 1, 0],
+        ease: 'inOut(2)'
+      }).then(() => {
+        $animeBox.removeChild($el)
+      })
+    }
+  }
   const draw = () => {
+    // 每次重绘前清空背景元素和动画
     bgFlowerArr.forEach(({ $el, animation }) => {
       animation.revert()
       $body.removeChild($el)
@@ -67,17 +122,25 @@ onMounted(() => {
     bgFlowerArr = []
     createBgFlower()
   }
-  draw()
-  // 监听窗口尺寸改变
-  window.addEventListener('resize', () => {
-    // console.log(utils.$('.bg-flower').length)
+  // 页面加载完成时执行
+  window.addEventListener('load', () => {
     draw()
+  })
+  // 监听窗口尺寸改变重绘
+  window.addEventListener('resize', () => {
+    draw()
+  })
+  // 点击button事件
+  $button.addEventListener('click', () => {
+    createButtonAnimation()
   })
 })
 </script>
 <template>
   <div class="body">
-    <div class="button">🎉 click me</div>
+    <div class="anime-box">
+      <div class="button">🎉 click me</div>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -92,7 +155,12 @@ onMounted(() => {
   position: relative;
   perspective: 1000px;
 }
+.anime-box {
+  position: relative;
+}
 .button {
+  position: relative;
+  z-index: 2;
   user-select: none;
   font-size: 14px;
   padding: 15px 20px;
